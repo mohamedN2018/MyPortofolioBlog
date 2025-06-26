70e59b15539b76d75055511318abb73f734ac3c8
MohaMed NabiL <49358099+mohamedN2018@users.noreply.github.com>
2025-06-26 12:45:12 +0300

    accounts full function

--- MyPortofolioBlog/hero/models.py
+++ MyPortofolioBlog/hero/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Hero(models.Model):
+    title = models.CharField(max_length=255, )
+    content = models.TextField()
+    image_hero = models.ImageField(upload_to='media/hero/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    site_name = models.CharField(max_length=100, default="MySite")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Hero'
+        verbose_name_plural = 'Heroes'
+
+
--- MyPortofolioBlog/hero/views.py
+++ MyPortofolioBlog/hero/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def HeroPage(request):
+    Hero = Hero.objects.all()
+    return render(request, 'hero/heropage.html')
