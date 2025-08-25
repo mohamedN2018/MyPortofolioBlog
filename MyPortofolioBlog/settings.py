ececa8f4a65793aa33b1036d96b91f23205b2d17
MohaMed Nabil <mohamednabilpro2024@gmail.com>
2025-08-25 16:57:17 +0300

    allowing all'

--- MyPortofolioBlog/settings.py
+++ MyPortofolioBlog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9zh13!ft9arnr-je^1o45gb$^m(rh&hvu!)p1iq#e%%oh*vh8e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
