efe912f44f886a62c59835ad21c46caba4b1e530
MohaMed Nabil <mohamednabilpro2024@gmail.com>
2025-08-27 19:37:31 +0300

    my sector key

--- MyPortofolioBlog/settings.py
+++ MyPortofolioBlog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9zh13!ft9arnr-je^1o45gb$^m(rh&hvu!)p1iq#e%%oh*vh8e'
+SECRET_KEY = config('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
