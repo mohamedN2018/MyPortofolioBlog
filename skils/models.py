c8fccbb1cc4ffb8d817aa9df876dc27460045488
MohaMed Nabil <mohamednabilpro2024@gmail.com>
2025-08-30 22:50:47 +0300

    edit site information

--- skils/models.py
+++ skils/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class skils_main(models.Model):
     main_title = models.CharField(max_length=100)
-    description_skils = models.TextField(max_length=255, blank=True, null=True)
+    description_skils = models.TextField(max_length=550, blank=True, null=True)
 
     def __str__(self):
         return self.main_title
